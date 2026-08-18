38c8d405fcb449ae5585feaec6465bdc7eee3882
Kostya <vkn.murm@gmail.com>
2026-08-18 22:11:22 +0300

    Тонкая латунная рамка вокруг фото Егора в хиро-блоке
    Passe-partout эффект: тонкая brass-линия с небольшим отступом вокруг
    существующего фотобокса.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
           </p>
         </div>
         <div className="flex justify-start">
-          <div className="relative w-72 sm:w-80">
+          <div className="relative w-72 sm:w-80 p-2 border border-brass">
             <div className="relative aspect-[4/5] bg-paper-raised border border-line overflow-hidden">
               <Image
                 src="/images/egor-andreev.jpg"
